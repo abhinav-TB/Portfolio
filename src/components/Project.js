aa1f2386e4439bdca9a6381f9a617811cc45c1ad
abhinav-TB <abhinavtb@gmail.com>
2020-07-12 11:13:22 +0530

    project page completed

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,41 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
-import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = ({projects,title,showLink}) => {
-  return <h2>project copmonent</h2>
+import { FaGithubSquare, FaShareSquare, FaHome } from "react-icons/fa"
+const Project = ({description,title,github,stack,url,image,index}) => {
+  return (
+
+    <article className="project">
+      <Image fluid={image.childImageSharp.fluid}
+      className="project-img"/>
+     
+     <div className="project-info">
+       <span className="project-number">{index+1}</span>
+    
+     <h3>{title}</h3>
+     <p className="project-desc">
+       {description}
+     </p>
+     <div className='project-stack'>
+       {stack.map((item)=>{
+         return(
+         <span key ={item.id}>{item.title}</span>
+         )
+       })}
+     </div>
+     <div className="projects-links">
+       <a href={github}>
+         <FaGithubSquare className="project-icon"/>
+       </a>
+       <a href={url}>
+         <FaHome className="project-icon"/>
+       </a>
+     </div>
+     </div>
+  
+
+    </article>
+  )
 }
 
 Project.propTypes = {}
