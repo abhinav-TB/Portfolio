3c570a75152f217c8c9a2885b5588b42a4b7ea0e
abhinav-TB <abhinavtb@gmail.com>
2020-07-13 17:24:22 +0530

    blog page created

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,8 +4,36 @@ import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
 // ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({data:{allStrapiBlogs:{nodes:blogs}}}) => {
+  return (
+    <Layout>
+      <section classname ="blog-page">
+        <Blogs blogs={blogs} title="blog"/>
+      </section>
+    </Layout>
+  )
 }
 
 export default Blog
+
+export const query = graphql`
+  {
+    allStrapiBlogs(sort: {fields: date, order: DESC}, limit: 3) {
+      nodes {
+        desc
+        date(formatString: "MM Do, YYY")
+        slug
+        title
+        content
+        category
+        image {
+          childImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+      }
+    }
+  }
+`
