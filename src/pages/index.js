3e493212f3c598f9d6f63268097517c109400270
abhinav-TB <abhinavtb@gmail.com>
2020-07-16 19:17:24 +0530

    new change

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export const query = graphql`
         image{
 
           childImageSharp{
-            fluid(quality: 90){
+            fluid(maxWidth: 3080, quality: 100){
               ...GatsbyImageSharpFluid
             }
           }
