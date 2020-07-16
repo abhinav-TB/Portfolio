05b871ecf5cec43152f1b2f7df78783b4129827f
abhinav-TB <abhinavtb@gmail.com>
2020-07-16 19:32:36 +0530

    services description updated

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,18 +5,18 @@ export default [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Im interested in web development and system design. I am familiar with React , Node js , Expres js, Gatsby , strapi ,flask etc.. You can contact me for collaboration in any projects`,
   },
   {
     id: 2,
     icon: <FaBrain className="service-icon" />,
     title: "Deep Learning",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I am budding deeplearing engineer , I am famiiar with tools such us pytorch, sklearn ,tensorflow etc and libraries such as pandas, numpy and matplotlib`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
     title: "app development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `i am intersted in making cutting edge apps and with moder technologies . I can code in flutter and react native .if you have a good ideas or want to collaborate with me in any project contact me `,
   },
 ]
